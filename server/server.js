7fd5d8bd9d7ca3f94ad035e7ec0eb0a01449b154
P S K Abdul Nawaz <princenawaz007@gmail.com>
2023-06-14 22:48:50 +0530

    React SSR

---
+++ server/server.js
@@ -0,0 +1,44 @@
+import path from 'path';
+import fs from 'fs';
+
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
+import express from 'express';
+
+import App from '../src/App';
+
+const PORT = process.env.PORT || 3006;
+const app = express();
+
+const handleRequest = (req, res) => {
+    const app = ReactDOMServer.renderToString(
+        <StaticRouter location={req.url}>
+            <App />
+        </StaticRouter>
+    );
+    const indexFile = path.resolve('./build/index.html');
+
+    fs.readFile(indexFile, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Something went wrong:', err);
+            return res.status(500).send('Oops, better luck next time!');
+        }
+
+        return res.send(
+            data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+        );
+    });
+};
+
+// ...
+
+app.get('^/$', handleRequest);
+
+app.get('/about', handleRequest);
+
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
--- src/index.js
+++ src/index.js
@@ -3,13 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+    BrowserRouter,
+    // RouterProvider,
+    // createBrowserRouter,
+} from 'react-router-dom';
+// import process from 'process';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    // development build code
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+        <React.StrictMode>
+            {/* <RouterProvider router={router}></RouterProvider> */}
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </React.StrictMode>
+    );
+    console.log('Dev', process.env.NODE_ENV, process.env);
+} else {
+    // production build code
+    console.log('ssr', process.env.NODE_ENV, process.env);
+    ReactDOM.hydrateRoot(
+        document.getElementById('root'),
+        <React.StrictMode>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </React.StrictMode>
+    );
+}
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
